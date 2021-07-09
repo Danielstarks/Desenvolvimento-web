e4f8c86a65f576f1d19bf68e5f9eafc1229ca3bc
Daniel Stark <kamirates@gmail.com>
2021-07-09 19:17:45 -0300

    teste

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27c591d44afea254db18"; }
+/******/ 		__webpack_require__.h = function() { return "4436b195bb74de444c28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
