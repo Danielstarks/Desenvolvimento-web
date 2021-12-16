4190e5e840b3171a659cc811057a4d7a5d6c4d4e
DanielStark <kamirates@gmail.com>
2021-12-16 17:49:55 -0300

    atualizando

--- pages/three.js
+++ pages/three.js
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=>, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script src="three.js" ></script>
-    <script>
+function iniciar(){
+
+
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
@@ -24,9 +16,9 @@ var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 var cube = new THREE.Mesh(geometry.material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 50;
 renderer.render(scene.camera);
 
-    </script>
-</body>
-</html>
+
+}
+
