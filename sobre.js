9c9aeeadc19e525b9c31c84ac4a5583bafe166b2
DanielStark <kamirates@gmail.com>
2022-01-28 19:24:47 -0300

    ok

---
+++ sobre.js
@@ -0,0 +1,17 @@
+// javaScript
+import Link from 'next/link';
+
+function sobre(){
+    return (
+    <div>
+        <h1>Sobre</h1>
+        <Link href="/">
+            <a>Acessar página Home</a>
+        </Link>
+
+         </div>
+    )
+
+    }
+
+export default sobre
