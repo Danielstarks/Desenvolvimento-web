67b3eb8f5a53be1e2e0264b691f9457076963c96
Danielstark <kamirates@gmail.com>
2022-07-11 09:15:26 -0300

    add new arquivos

--- jogo/my-app/src/index.js
+++ jogo/my-app/src/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import './index.css';
 
+
+class Square extends React.Component {
+    render(){
+        return(
+            <button className="square" onclick={function(){
+                console.log('click');
+            }}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
 class Board extends React.Component {
     renderSquares(i) {
         return <Square value={i}/>;
     }
 }
 
-class Square extends React.Component {
-    render(){
-        return(
-            <button className="square">
-                {this.props.value}
-            </button>
-        );
-    }
-}
