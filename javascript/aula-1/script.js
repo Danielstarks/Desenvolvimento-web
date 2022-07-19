c06d4a9db12cde1d8fd8b6183d34bae9145b2aee
DanielStark <kamirates@gmail.com>
2022-07-19 11:45:13 -0300

    ok

--- javascript/aula-1/script.js
+++ javascript/aula-1/script.js
@@ -6,3 +6,29 @@
 + Funções e suas funcionalidades;]
 
 */
+
+//var nome = "Daniel stark";
+//var idade = 25;
+//var sexo = "masculino";
+
+//alert(nome + "-" + idade + "-" + sexo);
+//console.log(nome);
+
+//arrey
+
+//var lista = ["pera", "uva", "laranja"];
+//lista.push("uva");  adicionar
+//lista.pop(); retirar
+//console.log(lista.lenght); tamanho da lista
+//console.log(lista.resverse) reverter lista
+
+//console.log(lista.join(" -")) adiciionar elementos
+
+// Dicionario "{}"
+
+//var fruta =[{nome:"maça", cor:"vermelha", tipo:"gala"}, {nome:"uva", cor:"roxo", tipo:"rubi"}]
+
+ /*lista de dicionario "[{}]" ..*/
+
+//console.log(fruta.nome);
+//alert(fruta[1].nome);
