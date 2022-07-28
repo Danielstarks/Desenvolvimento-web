cdabbd521dd1d795ebec67f78e965805dd47c4cd
DanielStark <kamirates@gmail.com>
2022-07-28 11:13:52 -0300

    ok

--- javaScript/curso/script.js
+++ javaScript/curso/script.js
@@ -1,34 +1,18 @@
-/*Estrutura basica de java script
 
-+ variaveis e seus valores;
-+ Vetores e Objetos;
-+ Estruturas condicionais;
-+ Funções e suas funcionalidades;]
+/* 
+ var: Declara uma variável, opcionalmente, inicializando-a com um valor.
+Experimental.
+ 
+let: Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+Experimental 
 
+const: Declare uma constante de escopo de bloco, apenas de leitura.
+ 
 */
 
-//var nome = "Daniel stark";
-//var idade = 25;
-//var sexo = "masculino";
 
-//alert(nome + "-" + idade + "-" + sexo);
-//console.log(nome);
+function greetMe(nome){
+    alert("Olá " + nome);
+}
 
-//arrey
-
-//var lista = ["pera", "uva", "laranja"];
-//lista.push("uva");  adicionar
-//lista.pop(); retirar
-//console.log(lista.lenght); tamanho da lista
-//console.log(lista.resverse) reverter lista
-
-//console.log(lista.join(" -")) adiciionar elementos
-
-// Dicionario "{}"
-
-//var fruta =[{nome:"maça", cor:"vermelha", tipo:"gala"}, {nome:"uva", cor:"roxo", tipo:"rubi"}]
-
- /*lista de dicionario "[{}]" ..*/
-
-//console.log(fruta.nome);
-//alert(fruta[1].nome);
+greetMe("mundo"); // " OLÁ MUNDO JAVASCRIPT"
