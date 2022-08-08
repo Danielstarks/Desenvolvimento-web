2ec49221f89a68f9f7b67c6bf22d3f5de036aaa1
DanielStark <kamirates@gmail.com>
2022-08-08 10:41:37 -0300

    add new project

--- javaScript/curso/script.js
+++ javaScript/curso/script.js
@@ -59,15 +59,85 @@ const: Declare uma constante de escopo de bloco, apenas de leitura.
 
 // dizerNome();
 
-// "OBJETO"
+// "Operdores aritiméticos"
 //===============================
 
-// "OBJETO"
+// let salario = 100;
+
+// // + , - , * , / , **
+
+// console.log(salario + salario);
+// console.log(salario - salario);
+// console.log(salario * salario);
+// console.log(salario / salario);
+// console.log(5 ** 5);
+
+// let idade = 18;
+
+// console.log(idade--);
+// console.log(idade++);
+
+// "OPERADORES TERNARIO"
 //===============================
 
-// "OBJETO"
+//tem um cliente, 100 premium, comum
+
+// let pontos= 200;
+// let tipo = pontos > 200 ? 'premium' : 'comum';
+// console.log(tipo);
+
+// "OPERADORES LÓGICOS"
 //===============================
 
-// "OBJETO"
+//Operador(&&) AND
+//Retorna true se os dois operadores forem true
+
+console.log(false && false);
+
+//Operador(||) OU
+//Retorna true se um dos operadores forem true
+// let maiorIdade = false;
+// let carteira = true;
+// let aplicar = maiorIdade || carteira;
+
+// console.log('Pode Aplicar:',aplicar);
+
+// //Operador(!) NOT
+// //Retorna Negado para dois valores false
+
+// let candidato = !aplicar;
+
+// console.log('Candidato Recusado',candidato);
+
+// "COMPARAÇÕES"
 //===============================
 
+// Se: IF e Senão: ELSE 
+// Switch ..case
+
+// if(valor){
+//  //primeira condição
+// }else if(valor){
+// //segunda condição
+// }else{
+// //fechamento de validação
+// }
+
+// let permissao; //comum,gerente,diretor
+
+// switch (permissao){
+
+//     case 'comum': 
+//     console.log('Usuário comum');
+//     break;
+
+//     case 'gerente': 
+//     console.log('Usuário gerente');
+//     break;
+
+//     case 'diretor': 
+//     console.log('Usuário diretor');
+//     break;
+
+//     default: console.log('Usuário não reconhecido!');
+// }
