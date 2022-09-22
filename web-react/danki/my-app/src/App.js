476d417cbde514b5ce5c3c11e501ad33edeb154b
DanielStark <kamirates@gmail.com>
2022-09-22 19:01:47 -0300

    ok

--- web-react/danki/my-app/src/App.js
+++ web-react/danki/my-app/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+function App() {
+
+
+  
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Olá mundo</h2>
       </div>
     );
-  }
+  
 }
 
 export default App;
