a018774d8e8b2f41ce7cc730d37053b9cee873c3
DanielSantos <kamirates@gmail.com>
2022-10-22 23:32:10 -0300

    ok

--- web-project/curso/script.js
+++ web-project/curso/script.js
@@ -5,21 +5,23 @@ var comecar = document.getElementById('comecar');
 
 
 var cronometroSeg;
+
+var minutoAtual;
 var segundoAtual;
 var interval;
 
 for (var i = 0; i <= 60; i++) {
-    minutos.innerHTML += '<option value="' + i + '">' + i + '</option>';
+    minutos.innerHTML+= '<option value="' + i + '">' + i + '</option>';
 }
 for (var i = 1; i <= 60; i++) {
-    segundos.innerHTML += '<option value="' + i + '">' + i + '</option>';
+    segundos.innerHTML+= '<option value="' + i + '">' + i + '</option>';
 }
 
 comecar.addEventListener('click', function () {
     minutoAtual = minutos.value;
     segundoAtual = segundos.value;
 
-    display.childNodes[1].innerHTML = minutosAtual + ":" + segundoAtual;
+    display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
 
     interval = setInterval(function () {
         segundoAtual--;
@@ -33,8 +35,9 @@ comecar.addEventListener('click', function () {
                 clearInterval(interval);
             }
         }
+        display.childNodes[1].innerHTML = minutoAtual + ":" + segundoAtual;
 
-    }, 1000);
+    },1000);
 
 
 })
