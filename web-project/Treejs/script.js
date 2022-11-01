a9f06582e0b5b35466d3b39ce874b61fc2478194
DanielSantos <kamirates@gmail.com>
2022-11-01 10:37:14 -0300

    ok

--- web-project/Treejs/script.js
+++ web-project/Treejs/script.js
@@ -1,6 +1,122 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera (75, window.innerWidth / window.innerHeight, 0,1, 1000);
+// var valor global;
+// let valor escopo de bloco;
+// const valor fixo;
 
-const renderer = new THREE.WebGLRenderer (scene);
-renderer.setSize (window.innerWidth / window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// função tradicional
+// function teste(){
+//     console.log('daniel');
+// }
+// teste();
+
+// //arow function
+// const nome = () => {
+//     console.log('bin');
+// }
+
+// nome();
+
+// // async
+// (
+//  function(){
+//     alert('Olá');
+//  }
+// )();
+
+// Callback function
+
+// function teste(nome, func){
+// console.log(nome);
+// par = {
+// nome: 'bin'
+// };
+// func(par);
+// }
+
+// teste('daniel',function(par){
+//    alert(par.nome);
+// });
+
+//Class js
+
+//class teste{
+
+/*constructor(){
+   this.nome = 'bin';
+  }*/
+
+//    constructor(nome){
+//     this.nome = nome;
+//     console.log(this.nome);
+//    }
+
+//    printNome(){
+//   //  console.log(this.nome);
+//    }
+
+// }
+
+// var instancia = new teste('ingridy');
+
+/*instancia.nome = 'daniel';
+
+instancia.printNome();
+
+var instancia2 = new teste();
+
+instancia2.printNome(); */
+
+//promise
+
+// function teste() {
+//     return new Promise(function (resolve, reject) {
+
+//         if(1 == 2){
+//             resolve('danki');
+//         }else{
+//             reject('falhou')
+//         }
+
+//     });
+// }
+
+// teste().then(function(val) {
+//     alert(val);
+// }).catch(function (error) {
+//     alert(error);
+// })
+
+
+// //async function
+
+// function teste(){
+
+//     return new  Promise(function(resolve, reject) {
+
+//         setTimeout(resolve,2000, 'primeiro')
+
+//     });
+// }
+// function teste2() {
+
+//     return new Promise(function (resolve, reject) {
+
+//         setTimeout(resolve, 1000, 'segundo')
+
+//     });
+// }
+
+
+// (
+//     async function(){
+
+//        await teste().then(function(val){
+//             console.log(val);
+//         });
+        
+//        await teste2().then(function (val) {
+//             console.log(val);
+//         });
+
+//         console.log('depois');
+//     }
+// )();
